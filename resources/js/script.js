58ff15fe4022ae1972cdec3f65b0be91b8888d93
gdoleon <35116635+gdoleon@users.noreply.github.com>
2020-02-11 21:15:58 +0000

    menu bar scroll/click interaction working

--- resources/js/script.js
+++ resources/js/script.js
@@ -21,3 +21,19 @@ const sectionOneObserver = new IntersectionObserver
 sectionOneOptions);
 
 sectionOneObserver.observe(sectionOne);
+
+// Header background change
+// $('.menu-btn').click(function() {
+//  if(clicked){//
+//    $('header').css('background-color', 'white');
+//    clicked = false;
+//  } else {
+//   $('header').css('background-color', 'rgba(0, 0, 0, 0.05);');
+//   clicked = true;
+//  }
+//});
+
+$("input:checkbox").click(function() {
+    var actualTime = "";
+    $(this).parent().toggleClass("header-scrolled-2");
+});
